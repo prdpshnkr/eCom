ea9f5e42182e8e4840d93b732c722ef083874e02
pradeepitpeople <pradeep.shankaraiah@itpeoplecorp.com>
2021-02-18 16:08:30 +0530

    cart summary

--- client/src/components/CartModal.js
+++ client/src/components/CartModal.js
@@ -3,12 +3,13 @@ import React from "react";
 import Modal from "react-modal";
 import {useShoppingCart} from "use-shopping-cart"; 
 import CartItem from './CartItem';
+import useCheckout from "../utils/useCheckout";
 
 Modal.setAppElement("#root");
 
 export default function CartModal({ isOpen, toggleModal }) {
   const {cartCount, formattedTotalPrice, cartDetails} = useShoppingCart();
-
+  const handleCheckout = useCheckout();
   const cartItems = Object.keys(cartDetails).map(key=> cartDetails[key]);
   
   return (
@@ -32,6 +33,7 @@ export default function CartModal({ isOpen, toggleModal }) {
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 style={{ marginRight: 4 }}
+                onClick={handleCheckout}
               >
                 Checkout Now
               </button>
--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require("express");
-const products = require("./products.json")
+const products = require("./products.json");
+const stripe = require("stripe")(process.env.STRIPE_API_SECRET);
+const {validateCartItems} = require('use-shopping-cart/src/serverUtil')
 
 module.exports = function getRoutes() {
   const router = express.Router();
   router.get('/products', getProducts);
   router.get('/products/:productId', getProduct);
+  router.post('/checkout-sessions', createCheckoutSession)
 
   return router;
 };
@@ -25,5 +28,28 @@ function getProduct(req, res) {
     res.status(200).json({product})
   } catch (error) {
     return res.status(404).json({statusCode: 404, message: error.message})
+  }
+}
+
+async function createCheckoutSession(req, res) {
+  try {
+    const cartItems = req.body;
+    const line_items = validateCartItems(products, cartItems);
+    const origin = process.env.NODE_ENV === 'production' ? req.headers.origin : 'http://localhost:3006'
+    const params = {
+      submit_type: 'pay',
+      payment_method_types: ['card'],
+      billing_address_collection: 'auto',
+      shipping_address_collection: {allowed_countries: ['US', 'CA']},
+      line_items,
+      success_url:`${origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: origin,
+      mode: 'payment',
+    }
+
+    const checkoutSession = await stripe.checkout.sessions.create(params);
+    res.status(200).join(checkoutSession)
+  } catch (error) {
+    res.status(500).json({statusCode:500, message:error.message})
   }
 }
